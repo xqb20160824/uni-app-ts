5458b645c604fc97d5e06b1abe406c98283dec88
xiangqb <xiangqibo@rioalc.com>
2023-03-22 11:52:28 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+const name=ref("asd");
+onLaunch(() => {
+  console.log("App Launch");
+});
+onShow(() => {
+  console.log("App Show");
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style></style>
